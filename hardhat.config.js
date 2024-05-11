c216429b3c0f00f4c9bbc04708406dce2919411a
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2024-05-11 23:17:00 +0545

    Updated ethers version

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+	defaultNetwork: 'hardhat',
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+	},
+	solidity: '0.8.24',
 };
